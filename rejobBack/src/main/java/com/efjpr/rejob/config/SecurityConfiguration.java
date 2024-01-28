d77deb9fc097cc0243b0b395fddcb4f0e0d05530
Pedro Tenorio <pedro.tenorio@audsat.com.br>
2024-01-28 20:50:46 -0300

    permitall to see jobs details

--- rejobBack/src/main/java/com/efjpr/rejob/config/SecurityConfiguration.java
+++ rejobBack/src/main/java/com/efjpr/rejob/config/SecurityConfiguration.java
@@ -46,7 +46,7 @@ public class SecurityConfiguration {
         http.csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(HttpMethod.GET, "/api/v1/jobs").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/jobs/**").permitAll()
                         .requestMatchers("/api/v1/auth/**", "/error").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
