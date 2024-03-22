b09c41204efc8ceee08ecd9d933cb042d3abcb05
FredericoFilho <fgogf@ic.ufal.br>
2024-03-22 20:59:12 -0300

    Mudanças na AuthControllerTest

--- rejobBack/src/test/java/com/efjpr/rejob/controller/AuthControllerTest.java
+++ rejobBack/src/test/java/com/efjpr/rejob/controller/AuthControllerTest.java
@@ -1,27 +1,22 @@
 package com.efjpr.rejob.controller;
 
-import com.efjpr.rejob.domain.Dto.AuthRequest;
-import com.efjpr.rejob.domain.Dto.AuthResponse;
-import com.efjpr.rejob.domain.Dto.AuthResponseCompany;
-import com.efjpr.rejob.domain.Dto.CollaboratorRegisterRequest;
-import com.efjpr.rejob.domain.Dto.CompanyRegisterRequest;
-import com.efjpr.rejob.domain.Dto.EmployeeRegisterRequest;
+import com.efjpr.rejob.controller.AuthController;
+import com.efjpr.rejob.domain.Dto.*;
 import com.efjpr.rejob.service.AuthService;
+import com.efjpr.rejob.service.CompanyService;
 import com.efjpr.rejob.service.email.EmailService;
+import com.efjpr.rejob.domain.Company;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
-public class AuthControllerTest {
+class AuthControllerTest {
 
     @Mock
     private AuthService authService;
@@ -29,67 +24,75 @@ public class AuthControllerTest {
     @Mock
     private EmailService emailService;
 
+    @Mock
+    private CompanyService companyService;
+
     @InjectMocks
     private AuthController authController;
 
     @Test
     void testRegisterEmployee() {
+        MockitoAnnotations.openMocks(this);
         EmployeeRegisterRequest request = new EmployeeRegisterRequest();
-        AuthResponse response = new AuthResponse();
-
+        AuthResponse response = new AuthResponse("token");
         when(authService.register(request)).thenReturn(response);
 
         ResponseEntity<AuthResponse> result = authController.register(request);
 
         assertEquals(HttpStatus.OK, result.getStatusCode());
         assertEquals(response, result.getBody());
+        verify(authService, times(1)).register(request);
     }
 
     @Test
     void testRegisterCollaborator() {
+        MockitoAnnotations.openMocks(this);
         CollaboratorRegisterRequest request = new CollaboratorRegisterRequest();
-        AuthResponse response = new AuthResponse();
-
+        AuthResponse response = new AuthResponse("token");
         when(authService.register(request)).thenReturn(response);
 
         ResponseEntity<AuthResponse> result = authController.register(request);
 
         assertEquals(HttpStatus.OK, result.getStatusCode());
         assertEquals(response, result.getBody());
+        verify(authService, times(1)).register(request);
     }
 
-// Teste comentado pois a permissão de acesso a AuthResponseCompany é restrita
 //    @Test
 //    void testRegisterCompany() {
+//        MockitoAnnotations.openMocks(this);
 //        CompanyRegisterRequest request = new CompanyRegisterRequest();
-//        AuthResponseCompany response = new AuthResponseCompany();
-//
+//        AuthResponseCompany response = new AuthResponseCompany("token", new Company());
 //        when(authService.register(request)).thenReturn(response);
 //
 //        ResponseEntity<AuthResponseCompany> result = authController.register(request);
 //
 //        assertEquals(HttpStatus.OK, result.getStatusCode());
 //        assertEquals(response, result.getBody());
+//        verify(authService, times(1)).register(request);
 //    }
 
     @Test
     void testAuthenticate() {
-        AuthRequest request = new AuthRequest();
-        AuthResponse response = new AuthResponse();
-
+        MockitoAnnotations.openMocks(this);
+        AuthRequest request = new AuthRequest("email", "password");
+        AuthResponse response = new AuthResponse("token");
         when(authService.authenticate(request)).thenReturn(response);
 
         ResponseEntity<AuthResponse> result = authController.authenticate(request);
 
         assertEquals(HttpStatus.OK, result.getStatusCode());
         assertEquals(response, result.getBody());
+        verify(authService, times(1)).authenticate(request);
     }
 
     @Test
     void testSendEmail() {
+        MockitoAnnotations.openMocks(this);
+
         String result = authController.SendEmail();
 
-        verify(emailService).sendSimpleMessage("fgogf@ic.ufal.br", "Teste", "Teste");
         assertEquals("ok", result);
+        verify(emailService, times(1)).sendSimpleMessage(anyString(), anyString(), anyString());
     }
 }
