0c3692c938966a2cd7e4d9e88adb78d9f2b6b5f2
FredericoFilho <fgogf@ic.ufal.br>
2024-03-25 21:43:53 -0300

    Adicionando server do Swagger UI no ambiente de produção

--- rejobBack/src/main/java/com/efjpr/rejob/config/OpenApiConfig.java
+++ rejobBack/src/main/java/com/efjpr/rejob/config/OpenApiConfig.java
@@ -7,13 +7,28 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
         info = @Info(
+                contact = @Contact(
+                        email = "mailrejob@gmail.com",
+                        url = "http://rejob.edge.net.br"
+                ),
                 description = "Documentação OpenApi do sistema ReJob",
                 title = "Especificações OpenApi",
-                version = "1.0"
+                version = "1.0.0"
         ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "http://rejob.edge.net.br"
+                )
+        },
         security = {
                 @SecurityRequirement(
                         name = "bearerAuth"
